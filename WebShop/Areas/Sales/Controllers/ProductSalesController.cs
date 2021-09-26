89dd95497decc394342a78621afc76c83ef1e7d5
louki-vn <61510142+louki-vn@users.noreply.github.com>
2021-09-26 08:41:33 +0700

    c

--- WebShop/Areas/Sales/Controllers/ProductSalesController.cs
+++ WebShop/Areas/Sales/Controllers/ProductSalesController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using WebShop.Models;
 
--- WebShop/Areas/Sales/Controllers/RespondComplainController.cs
+++ WebShop/Areas/Sales/Controllers/RespondComplainController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
-using System.Text;
-using System.Web;
-using System.Web.Helpers;
 using System.Web.Mvc;
 using WebShop.Models;
 
